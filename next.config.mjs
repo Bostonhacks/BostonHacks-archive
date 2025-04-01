e149afa1d1daab01a5cfe30474317ef41d48f8ed
declanyg <declan@youngs.asia>
2025-04-01 01:28:45 -0400

    Added BostonHacks 2023 and 2024 websites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/2023',
+            destination: '/2023/index.html',
+          },
+          {
+            source: '/2024',
+            destination: '/2024/index.html',
+          },
+        ];
+      },
+};
 
 export default nextConfig;
